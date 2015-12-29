5f06fede1f4cfa1391b0395b7ca673aa1ebeec95
r_yamada <yamada@iss-tokyo.co.jp>
2015-12-29 15:30:35 +0900

    Render building list in Home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import buildingService from '../buildingService';
 
 export default class Home extends React.Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      buildings: []
+    };
+    this.fetchBuildings();
+  }
+
   render() {
+    const { buildings } = this.state;
     return (
       <div>
-        <h1>Not implemented yet..</h1>
-        <a href="/#/building/1">Sample Building</a>
+        <h1>Sample buildings</h1>
+        <ul className="buildings">
+          { buildings.map(this.renderBuilding) }
+        </ul>
       </div>
     );
+  }
+
+  renderBuilding({ id, name }) {
+    return (
+      <li key={id}>
+        <a href={`/#/building/${id}`}>{name}</a>
+      </li>
+    )
+  }
+
+  fetchBuildings() {
+    buildingService.listAll()
+      .then(buildings => {
+        this.setState({ buildings });
+      });
   }
 }
